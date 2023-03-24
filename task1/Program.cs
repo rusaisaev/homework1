ae124e249ad29867e8cebac5a5f1ff4cdd93bc06
ruslan <rusya.isaev.06@mail.ru>
2023-03-24 14:46:19 +0300

    complite

---
+++ task1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число ");
+int b = int.Parse(Console.ReadLine()!);
+if(a > b) Console.WriteLine(a);
+else if (a < b) Console.WriteLine(b);
+else Console.WriteLine("==");
