ae877137a171ebde9893a5d0df223ebc9e4b43aa
ruslan <rusya.isaev.06@mail.ru>
2023-03-24 15:20:24 +0300

    cpmplite

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число ");
+int c = int.Parse(Console.ReadLine()!);
+// приняли числа 
+int max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+else Console.WriteLine("==");
+
+Console.WriteLine(max);
+ 
+
+/* - if(a > b) Console.WriteLine(a);
+else if(b > a) Console.WriteLine(b);
+else if(a > c) Console.WriteLine(a);
+else if(c > a) Console.WriteLine(c);
+else if(b > c) Console.WriteLine(b);
+else if(c > b) Console.WriteLine(c);
+else Console.WriteLine("==");
+*/
